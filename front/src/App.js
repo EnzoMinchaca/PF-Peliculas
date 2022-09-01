6ad018915e8882ed8f03a19716983ac6ebc510c6
Davlopez06 <davidlopezvaldes@gmail.com>
2022-09-01 18:31:20 -0500

    Cards

--- front/src/App.js
+++ front/src/App.js
@@ -6,13 +6,14 @@ import Form from "./components/AdmidView/Form"
 import Car from "./components/UserView/Car"
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Order from './components/UserView/Order';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/Home" element={<Home/>} />
-        <Route exact path="/Details" element={<Details/>}/>
+        <Route exact path="/Details/:id" element={<Details/>}/>
         <Route exact path="/Form" element={<Form/>}/>
         <Route exact path="/Car" element={<Car/>}/>
       </Routes>
