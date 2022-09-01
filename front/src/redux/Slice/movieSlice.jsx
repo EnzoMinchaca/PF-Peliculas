5fca8e3cb5b0d866d97bb366da0a5e1e484fd116
yusbelypv <yusbely@gmail.com>
2022-09-01 16:46:02 -0500

    Redux Toolkit

---
+++ front/src/redux/Slice/movieSlice.jsx
@@ -0,0 +1,52 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+
+export const movieSlice = createSlice ({
+    name: "movies",
+    initialState:{
+        movies:[],
+        movie: {},
+        filtered: [],
+        genres: [],
+        platform: []
+    },
+    
+    reducer: {
+        getAllMovies: (state, action)=>{
+            state.movies = action.payload
+            state.filtered = action.payload
+
+        },
+
+        getMoviesById: (state, action)=>{
+            state.movie = action.payload
+        },
+
+        getSearchMovie: (state, action)=>{
+            state.movies = action.payload
+        },
+
+       /* getPostMovie: (state)=>{
+            state
+        },*/
+
+        getByGenres: (state, action)=>{
+            state.genres = action.payload
+        },
+
+        getByPlatform: (state, action)=>{
+            state.platform = action.payload
+        }
+    }
+
+})
+
+export const { 
+    getAllMovies, 
+    getMoviesById,
+    getSearchMovie,
+    getByGenres,
+    getByPlatform
+} = movieSlice.actions
+
+export default movieSlice.reducer
