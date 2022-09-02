84bf11f6b8c48093c3c1bc5936704512e4859bb9
Davlopez06 <davidlopezvaldes@gmail.com>
2022-09-02 10:36:10 -0500

    Cards

--- front/src/components/UserView/Card.jsx
+++ front/src/components/UserView/Card.jsx
@@ -7,8 +7,8 @@ import { CardActionArea } from '@mui/material';
 import css from "./Card.module.css";
 import { Link } from "react-router-dom";
 
-export default function CardIndividual(props){
-    return(
+export default function CardIndividual(props) {
+  return (
     //     <Card sx={{ maxWidth: 345 }} className={props.platform.toLowerCase()==="amazon" ? css.amazon :
     //     props.platform.toLowerCase()==="netflix" ? css.netflix:
     //     props.platform.toLowerCase()==="disney+" ? css.disney:
@@ -34,40 +34,40 @@ export default function CardIndividual(props){
     //   </Card>
 
 
-//////////////////////////////////////////////////////////////////////////////
-    <Link to={`/Details/${props.id}`}>
-    <div className={props.platform.toLowerCase()==="amazon" ? css.cardA :
-                  props.platform.toLowerCase()==="netflix" ? css.cardN:
-                  props.platform.toLowerCase()==="disney+" ? css.cardD:
-                  props.platform.toLowerCase()==="hbomax" ? css.cardH: css.cardP}>
-    <div className={css.blob}></div>
-      <span className={css.image}><img src={props.image} className={css.img}/></span>
-      <h2 className={css.title}><span>{props.title}</span></h2>
-      <p className={css.flex}>
-       {props.genres?.map(genre=>{
-        return (<p className={css.icon}>
-        | {genre} |
-      </p>)
-      })}
-      </p>
-    </div>
+    //////////////////////////////////////////////////////////////////////////////
+    <Link className={css.textDecoration} to={`/Details/${props.id}`}>
+      <div className={props.platform.toLowerCase() === "amazon" ? css.cardA :
+        props.platform.toLowerCase() === "netflix" ? css.cardN :
+          props.platform.toLowerCase() === "disney+" ? css.cardD :
+            props.platform.toLowerCase() === "hbomax" ? css.cardH : css.cardP}>
+        <div className={css.blob}></div>
+        <span className={css.image}><img src={props.image} className={css.img} /></span>
+        <h2 className={css.title}><span>{props.title}</span></h2>
+        <p className={css.flex}>
+          {props.genres?.map(genre => {
+            return (<p className={css.icon}>
+              | {genre} |
+            </p>)
+          })}
+        </p>
+      </div>
     </Link>
 
     //////////////////////////////////////////////////////////////////////////////
 
-//     <div className={props.platform.toLowerCase()==="amazon" ? css.cardA :
-//            props.platform.toLowerCase()==="netflix" ? css.cardN:
-//            props.platform.toLowerCase()==="disney+" ? css.cardD:
-//            props.platform.toLowerCase()==="hbo" ? css.cardH: css.cardP}>
-//   <div className={css.img}>
-//     <img src={props.image} alt={props.image} className={css.image}/>
-//   </div>
-//   <h1>{props.title}</h1>
-//   <p>
-//     |{props.genres?.map(genre=>{
-//         return ` ${genre} |`
-//     })}
-//   </p>
-// </div>
-    )
+    //     <div className={props.platform.toLowerCase()==="amazon" ? css.cardA :
+    //            props.platform.toLowerCase()==="netflix" ? css.cardN:
+    //            props.platform.toLowerCase()==="disney+" ? css.cardD:
+    //            props.platform.toLowerCase()==="hbo" ? css.cardH: css.cardP}>
+    //   <div className={css.img}>
+    //     <img src={props.image} alt={props.image} className={css.image}/>
+    //   </div>
+    //   <h1>{props.title}</h1>
+    //   <p>
+    //     |{props.genres?.map(genre=>{
+    //         return ` ${genre} |`
+    //     })}
+    //   </p>
+    // </div>
+  )
 }
