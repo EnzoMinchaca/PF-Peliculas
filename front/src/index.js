300aa237a938c63476c9ab25cb05ea7d1f101e2f
yusbelypv <yusbely@gmail.com>
2022-09-06 17:52:15 -0500

    LocalStorage

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from './redux/Store';
+import axios from 'axios';
+
+axios.defaults.headers.common = { 'Authorization': `bearer ${localStorage.getItem('token')}` }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
