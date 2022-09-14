638b98e9d2f76c335bc6b258a68b71ca79f753d1
vova45 <94209953+vova45@users.noreply.github.com>
2022-09-14 14:31:34 -0300

    emails-baneo-promocion-compra

--- api/src/config/emailer.js
+++ api/src/config/emailer.js
@@ -1,13 +1,6 @@
 const nodemailer = require("nodemailer")
 const { google } = require("googleapis")
 
-/* const email = process.env.EMAIL
-const pass = process.env.PASSWORD
-const cliendId = process.env.CLIENTID
-const clientSecret = process.env.CLIENTSECRET
-const refreshToken = process.env.REFRESHTOKEN
-const accessToken = process.env.ACCESSTOKEN */
-
     const CLIENT_ID = "435765551994-0dvbqrs6g6k3qbjr7lfnr3ds2vanuc39.apps.googleusercontent.com"
     const CLIENT_SECRET = "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"
     const REDIRCET_URI = "https://developers.google.com/oauthplayground"
@@ -48,7 +41,8 @@ module.exports.sendConfirmationEmail = async (name, email, confirmationCode) => 
           </div>`,
     }).catch(err => console.log(err));
   };
-module.exports.RetrievePassword =async  ( email,verificationLink ) => {
+  
+module.exports.RetrievePassword = async  ( email,verificationLink ) => {
   console.log("Check");
 
   oAuth2Client.setCredentials({ refresh_token: REFRESHTOKEN})
@@ -82,7 +76,7 @@ module.exports.RetrievePassword =async  ( email,verificationLink ) => {
     })
   }
 
-module.exports.SendPuchase = async ( email, nameMovie, date, hour, linkViewMovie, price, image ) => {
+module.exports.SendPuchase = async ( email, array ) => {
   console.log("Check")
 
   oAuth2Client.setCredentials({ refresh_token: REFRESHTOKEN})
@@ -104,109 +98,382 @@ module.exports.SendPuchase = async ( email, nameMovie, date, hour, linkViewMovie
     }
   })
   //transporter.verify().then(data=> console.log(data)).catch(error=> console.log(error))
+  let imagenes = "";
+  let nameMovies = "";
+  let date = "";
+  let hour = "";
+  let price = "";
+  let links = "";
+  let total = array.length
+  let value = "";
+  if(array){
+    
+    imagenes = array.map(e =>{
+      return `<td><img src=${e.image}></td>`
+    }).join().split("").filter(e => {
+      if(e !== ","){
+        return e
+      }
+    }).join("")
+    nameMovies = array.map(e =>{
+      return `<td>Title: ${e.nameMovie}</td>`
+    }).join().split("").filter(e => {
+      if(e !== ","){
+        return e
+      }
+    }).join("")
+    date = array.map(e =>{
+      return `<td>Date: ${e.date}</td>`
+    }).join().split("").filter(e => {
+      if(e !== ","){
+        return e
+      }
+    }).join("")
+    hour = array.map(e =>{
+      return `<td>Hour: ${e.hour}</td>`
+    }).join().split("").filter(e => {
+      if(e !== ","){
+        return e
+      }
+    }).join("")
+    price = array.map(e =>{
+      return `<td>Price: $${e.price}</td>`
+    }).join().split("").filter(e => {
+      if(e !== ","){
+        return e
+      }
+    }).join("")
+
+    links = array.map(e => {
+      return `<a href=${e.linkViewMovie} style="margin-left:10px ;margin-left: 10px; font-size: 20px;">Watch ${e.nameMovie}</a>`
+    }).join().split("").filter(e => {
+      if(e !== ","){
+        return e
+      }
+    }).join("")
+
+    value = array.map(e => {
+      return Number(e.price)
+    }).reduce((acc, c)=>{
+      return acc+ c
+    })
+  }
+
+
   transporter.sendMail({
     from: "Puchase successful",
     to: email,
     subject: "Shipping confirmation of purchase.",
-    html:`
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-    <style>
-    td > h2 {
-      color: white;
-    }
-    h4{
-      font-size: 20px;
-      color: white
-    }
-    p{
-      font-size: 20px;
-    }
-    i{
-      font-size: 20px;
-    }
-    div > a{
-      margin-left: 10px;
-      margin-right: 10px;
-      font-size: 20px;
-    }
-    a{
-      text-decoration: none;   
-    }
-    table{
-      margin-top: 100px;
-    }
-    img{
-      max-height:300px; max-width: 220px ;
-    }
-    </style>
-</head>
-<body>
-<div style="width:100% ; background-color:  #2E86C1 ;">
-    <div style="background-color: rgba(0, 0, 0, 0.514);">
-        <!-- Imagen inicial -->
-        <div style="padding: 20px 10px 20px 10px; height: 100px; text-align:center" >
-            <div style="position:absolute ; left:50% ; transform: translate(-50%); top:3%">
-                <img src=cid:logo style="width: 100px; height: 80px; display: inline;">
-                <a target="_blank" href="https://localhost:3000/home"><h1 style="display: inline-block; color:black;">Movies Films.</h1></a>
+    html:`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+        <style>
+            h4{
+                font-size: 20px;
+                color: white
+            }
+            p{
+                font-size: 20px;
+                font: bold;
+            }
+            i{
+                font-size: 20px;
+            }
+            div > a{
+                font-size: 20px;
+            }
+            a{
+                text-decoration: none; 
+                font-size:20px;  
+            }
+            img{
+                height: 300px;
+            }
+            td{
+                font-size: 20px;
+                font: bold;
+            }
+            div > a {
+          }
+        </style>
+        
+    </head>
+    <body>
+    <div style="width:100% ; background-color:  #2E86C1 ;">
+        <div style="background-color: rgba(0, 0, 0, 0.514);">
+            <!-- Imagen inicial -->
+            <div style="padding: 20px 10px 20px 10px; height: 100px; text-align:center" >
+                <div style="position:absolute ; left:50% ; transform: translate(-50%); top:3%;">
+                    <img src="carrete.png" style="width: 100px; height: 80px; display: inline;">
+                    <a target="_blank" href="https://localhost:3000/home"><h1 style="display: inline-block; color:black;">Movies Films.</h1></a>
+                </div>
             </div>
-        </div>
-        <!-- Contenido Principal -->
-        <div style=" background-color: #D0D3D4 ; padding: 20px 10px 20px 10px ; text-align: center;">
-            <h1>¡Purchase made successfully!</h1>
-            <div style=" text-align: center ;height: 350px ; width: 100%; background-color: #5D6D7E; padding-top: 2%;">
-                <table style="text-align: center; margin: auto; ">
+            <!-- Contenido Principal -->
+            <div style=" background-color: #D0D3D4 ; padding: 20px 10px 20px 10px ; text-align: center ; ">
+                <div style=" text-align: center ;height: 800px ; width: 100%;">
+                    <h2>¡Congratulations these ${total} movies have been added to your gallery!</h2>
+                    <table id="table-base" style="text-align: center; margin: auto; background-color:#5D6D7E  ; padding: 20px 10px 20px 10px; border-radius: 2%; ">
                     <tr>
-                        <td rowspan="5"><img src=${image} style="border-radius: 5px;"></td>
+                        ${imagenes}
                     </tr>
                     <tr>
-                        <td ><h2>Title:</h2></td>
-                        <td ><h2>${nameMovie}</h2></td>
+                        ${nameMovies}
                     </tr>
                     <tr>
-                        <td><h4>Purchase date:</h4></td>
-                        <td><h4>${date}</h4></td>
+                        ${date}
                     </tr>
                     <tr>
-                        <td><h4>Time of purchase:</h4></td>
-                        <td><h4>${hour}</h4></td>
+                        ${hour}
                     </tr>
                     <tr>
-                        <td><h4>Price:</h4></td>
-                        <td><h4>$${price}</h4></td>
+                        ${price}
                     </tr>
-                </table>
+                    </table>
+                    <p>¡Successful purchase!</p>
+                    <p>This is the total cost of the movies : $${value}</p>
+                    <p>Here is the link of each of the movies:</p>
+                    <div>
+                        ${links}
+                    </div>
+                    <hr>
+                    <a href="https://localhost:3001/home">Go to website</a>
+                </div>
             </div>
-            <p>To watch the movie, go to the following link:</p>
-            <a href=${linkViewMovie}>Watch ${nameMovie}</a>
-            <!-- Agradecimiento -->
-            <p>Thanks for your purchase.</p>
-            
-        </div>
-
-
-        <div style=" padding: 20px 10px 20px ; color: black; text-align: center" >
-            <!-- Services -->
-            <p> Customer service: pruebaDatos89@gmail.com || © 2022 MoviesFilms. All Rights Reserved.</p>
+            <div style=" padding: 20px 10px 20px ; color: black; text-align: center" background-color:  #2E86C1>
+                <!-- Services -->
+                <i>Customer service: pruebaDatos89@gmail.com || © 2022 MoviesFilms. All Rights Reserved.</i>
+            </div>
         </div>
     </div>
-</div>
-</body>
-</html>
-    `,
+    </body>
+    </html>`,
     attachments:[
       {
         filename:"carrete.png",
         path:"./src/config/imageEmail/carrete.png",
         cid:"logo"
       }
-    ]
+    ],
+    
   }).catch(error=> console.log(error))
+}
+
+module.exports.BannedAccount = async ( name, lastname, email, banned ) => {
+  oAuth2Client.setCredentials({ refresh_token: REFRESHTOKEN})
+
+  let comunicate = [ "Your account has been banned.", "Your account has been unbanned."]
+  let response;
+  
+  if(banned === "true"){
+
+    response = comunicate[0]
+    
+  }else{
+
+    response = comunicate[1]
+  
+  }
+
+  const accessToken = await oAuth2Client.getAccessToken()
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+      type:"OAuth2",
+      user:"pruebadatos86@gmail.com",
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESHTOKEN,
+      accessToken: accessToken
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  })
+
+  transporter.sendMail({
+    from: "Banned Account",
+      to:email,
+      subject: "Account status",
+      html: `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title></title>
+          <style>
+              h4{
+                  font-size: 20px;
+                  color: white
+              }
+              p{
+                  font-size: 20px;
+                  font: bold;
+              }
+              i{
+                  font-size: 20px;
+              }
+              div > a{
+                  font-size: 20px;
+              }
+              a{
+                  text-decoration: none;   
+              }
+          </style>
+      </head>
+      <body>
+      <div style="width:100% ; background-color:  #2E86C1 ;">
+          <div style="background-color: rgba(0, 0, 0, 0.514);">
+              <!-- Imagen inicial -->
+              <div style="padding: 20px 10px 20px 10px; height: 100px; text-align:center" >
+              <div style="position:absolute ; left:50% ; transform: translate(-50%); top:3%;">
+              <img src=cid:logo style="width: 100px; height: 80px; display: inline;">
+                      <a target="_blank" href="https://localhost:3000/home"><h1 style="display: inline-block; color:black;">Movies Films.</h1></a>
+                  </div>
+              </div>
+              <!-- Contenido Principal -->
+              <div style=" background-color: #D0D3D4 ; padding: 20px 10px 20px 10px ; text-align: center">
+                  <p style="color:black ; font-size: 30px;">${response}</p>
+                  <p>Name: ${name} ${lastname} | Email: ${email}</p>
+                  <p>For more information on the matter, contact:</p>
+                  <p>pruebaDatos89@gmail.com</p>
+                  <hr>
+                  <a href="https://localhost:3000/home">Go to website</a>
+              </div>
+              <div style=" padding: 20px 10px 20px ; color: black; text-align: center" >
+                  <!-- Services -->
+                  <i>Customer service: pruebaDatos89@gmail.com || © 2022 MoviesFilms. All Rights Reserved.</i>
+              </div>
+          </div>
+      </div>
+      </body>
+      </html>
+      `,
+      attachments:[
+        {
+            filename:"carrete.png",
+            path:"./src/config/imageEmail/carrete.png",
+            cid:"logo"
+        }
+      ]
+  }).catch(error => console.log(error))
+}
+
+module.exports.PromotionAccount = async ( name, lastname, email , promocion ) => {
+  console.log("Check")
+
+  let comunicate = [ "Your account has been Promoted to:", "Your account has been downgraded to:"]
+  let response;
+
+  console.log(promocion+ "1")
+  console.log(name+ "2")
+  console.log(lastname+ "3")
+  console.log(email+ "4")
+  
+  
+  if( promocion === "Admin") {
+    response = comunicate[0]
+  }else{
+    response = comunicate[1]
+  }
+
+  
+
+  oAuth2Client.setCredentials({ refresh_token: REFRESHTOKEN})
+
+  const accessToken = await oAuth2Client.getAccessToken()
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+      type:"OAuth2",
+      user:"pruebadatos86@gmail.com",
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESHTOKEN,
+      accessToken: accessToken
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+    
+  })
+  transporter.verify().then(data=> console.log(data)).catch(error=> console.log(error))
+  transporter.sendMail({
+    from: "Promotion Account",
+      to:email,
+      subject: "Account status",
+      html:`<!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title></title>
+          <style>
+              h4{
+                  font-size: 20px;
+                  color: white
+              }
+              p{
+                  font-size: 20px;
+                  font: bold;
+              }
+              i{
+                  font-size: 20px;
+              }
+              div > a{
+                  font-size: 20px;
+              }
+              a{
+                  text-decoration: none;   
+              }
+              
+          </style>
+      </head>
+      <body>
+      <div style="width:100% ; background-color:  #2E86C1 ;">
+          <div style="background-color: rgba(0, 0, 0, 0.514);">
+              <!-- Imagen inicial -->
+              <div style="padding: 20px 10px 20px 10px; height: 100px; text-align:center" >
+                  <div style="position:absolute ; left:50% ; transform: translate(-50%); top:3%;">
+                      <img src=cid:logo style="width: 100px; height: 80px; display: inline;">
+                      <a target="_blank" href="https://localhost:3000/home"><h1 style="display: inline-block; color:black;">Movies Films.</h1></a>
+                  </div>
+              </div>
+              <!-- Contenido Principal -->
+              <div style=" background-color: #D0D3D4 ; padding: 20px 10px 20px 10px ; text-align: center">
+                  <h3>${response}</h3>
+                  <h2>${promocion}</h2>
+                  <p>Name: ${name} ${lastname} | Email: ${email}</p>
+                  <p>For more information on the matter, contact:</p>
+                  <p>pruebaDatos89@gmail.com</p>
+                  <hr>
+                  <a href="https://localhost:3000/home"Go to website</a>
+              </div>
+              <div style=" padding: 20px 10px 20px ; color: black; text-align: center" >
+                  <!-- Services -->
+                  <i>Customer service: pruebaDatos89@gmail.com || © 2022 MoviesFilms. All Rights Reserved.</i>
+              </div>
+          </div>
+      </div>
+      </body>
+      </html>`,
+      attachments:[
+        {
+          filename:"carrete.png",
+            path:"./src/config/imageEmail/carrete.png",
+            cid:"logo"
+        }
+      ]
+      
+  }).catch(error=> console.log(error))
+
 }
 
 
