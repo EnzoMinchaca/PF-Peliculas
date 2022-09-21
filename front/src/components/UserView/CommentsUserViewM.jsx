df4fd81beb17f496b0039ec5ac25eaf3030c1cbb
Emmanuel Sandoval <iemmanuel.sandovalv@gmail.com>
2022-09-20 21:21:29 -0500

    landing, scrollbarcoments

--- front/src/components/UserView/CommentsUserViewM.jsx
+++ front/src/components/UserView/CommentsUserViewM.jsx
@@ -6,18 +6,18 @@ import './CommentsUserViewM.css'
 import Swal from "sweetalert2";
 
 
-export default function CommentsUserVM ({username, rating, created_at, content, avatar_path, titleMovies}) {
- 
+export default function CommentsUserVM({ username, rating, created_at, content, avatar_path, titleMovies }) {
+
     const [show, setShow] = React.useState(false)
     const [isAdmin, setisAdmin] = React.useState(false)
     const dispatch = useDispatch()
     // console.log(titleMovies)
     React.useEffect(() => {
         const theuser = JSON.parse(localStorage.getItem('user'))
-        if(theuser.name === username) {
+        if (theuser.name === username) {
             setShow(true)
         }
-        if(theuser.isAdmin) {
+        if (theuser.isAdmin) {
             setisAdmin(true)
         }
     }, [])
@@ -52,69 +52,70 @@ export default function CommentsUserVM ({username, rating, created_at, content, 
         })
     }
 
-    let date= created_at.slice(0, -14);
+    let date = created_at.slice(0, -14);
     let stars;
     let image;
     // console.log(user)
-    if(avatar_path !== null){
-        if(avatar_path[0]+ avatar_path[1]+ avatar_path[2]+avatar_path[3]+avatar_path[4] ==='/http'){
-            image=avatar_path.slice(1);
-           
+    if (avatar_path !== null) {
+        if (avatar_path[0] + avatar_path[1] + avatar_path[2] + avatar_path[3] + avatar_path[4] === '/http') {
+            image = avatar_path.slice(1);
 
-        }else if(avatar_path[0]+ avatar_path[1]+ avatar_path[2]+avatar_path[3] ==='http'){
-            image=avatar_path;
-        } 
-        else{
-            image='https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1705/alekseyvanin170500978/77418653-usuario-icono-de-cuenta-plana-bot%C3%B3n-redondo-simple-signo-circular-vector-dise%C3%B1o-de-estilo-plano.jpg';
 
-         }
-    if(rating===0){
-       stars=0
-    }else if(rating===1 || rating===2){
-        stars=1
-    }else if(rating===3 || rating===4){
-        stars=2
-    }else if(rating===5 || rating===6){
-        stars=3
-    }else if(rating===7 || rating===8){
-        stars=4
-    }else if(rating===9 || rating===10){
-        stars=5
-    }
+        } else if (avatar_path[0] + avatar_path[1] + avatar_path[2] + avatar_path[3] === 'http') {
+            image = avatar_path;
+        }
+        else {
+            image = 'https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1705/alekseyvanin170500978/77418653-usuario-icono-de-cuenta-plana-bot%C3%B3n-redondo-simple-signo-circular-vector-dise%C3%B1o-de-estilo-plano.jpg';
 
-    return(
-       
+        }
+        if (rating === 0) {
+            stars = 0
+        } else if (rating === 1 || rating === 2) {
+            stars = 1
+        } else if (rating === 3 || rating === 4) {
+            stars = 2
+        } else if (rating === 5 || rating === 6) {
+            stars = 3
+        } else if (rating === 7 || rating === 8) {
+            stars = 4
+        } else if (rating === 9 || rating === 10) {
+            stars = 5
+        }
 
-          <div class="wrapper">
-          <ul>
-              {/* <!-- una vez que se responde a un mensaje --> */}
-             <li>
-                 <div class="responde">
-                   <div class="box">
-                                                        
-                      <img src={image}/>
-                
-                      <article>
-                      <div class="head">
-                        <h6>User {username} <Rating  name="read-only" size="small" value={stars} readOnly ></Rating></h6>
-                        <a>{date}</a>
-                        
-                      </div>
-                      <p class="cont">{content}.</p>
-                      <div class="bt">
-                        {
-                            show || isAdmin?
-                                <button onClick={(e) => handleClick(e)} class="butn">Delete</button>
-                            :
-                            null
-                        }
-                      </div>
-                      </article>
-                   </div>
-                 </div>
-             </li>
-          </ul>
+        return (
+
+
+            <div class="wrapper">
+                <ul>
+                    {/* <!-- una vez que se responde a un mensaje --> */}
+                    <li>
+                        <div class="responde">
+                            <div class="box">
+
+                                <img src={image} />
+
+                                <article>
+                                    <div class="head">
+                                        <h6>User {username} <Rating name="read-only" size="small" value={stars} readOnly ></Rating></h6>
+                                        <a>{date}</a>
+
+                                    </div>
+                                    <p class="cont">{content}.</p>
+                                    <div class="bt">
+                                        {
+                                            show || isAdmin ?
+                                                <button onClick={(e) => handleClick(e)} class="butn">Delete</button>
+                                                :
+                                                null
+                                        }
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-     
-    )
-};
+
+        )
+    }
+}
